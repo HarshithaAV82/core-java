005e65e9c7569effda512ae464d3a74e1a931360
HarshithaAV82 <avharshitha607@gmail.com>
2026-02-03 11:11:31 +0530

    Add files via upload

---
+++ GreenLine.java
@@ -0,0 +1,101 @@
+class GreenLine{
+	public static void main(String[] args) {
+
+		String station1 = "Madavara (BIEC)";
+		System.out.println(station1);
+
+		String station2 = "Chikkabidarakallu";
+		System.out.println(station2);
+
+		String station3 = "Manjunathnagar";
+		System.out.println(station3);
+
+		String station4 = "Nagasandra";
+		System.out.println(station4);
+
+		String station5 = "Dasarahalli";
+		System.out.println(station5);
+
+		String station6 = "Jalahalli";
+		System.out.println(station6);
+
+		String station7 = "Peenya Industry";
+		System.out.println(station7);
+
+		String station8 = "Peenya";
+		System.out.println(station8);
+
+		String station9 = "Goraguntepalya";
+		System.out.println(station9);
+
+		String station10 = "Yeshwanthpur";
+		System.out.println(station10);
+
+		String station11 = "Sandal Soap Factory";
+		System.out.println(station11);
+
+		String station12 = "Mahalakshmi";
+		System.out.println(station12);
+
+		String station13 = "Rajajinagar";
+		System.out.println(station13);
+
+		String station14 = "Kuvempu Road";
+		System.out.println(station14);
+
+		String station15 = "Srirampura";
+		System.out.println(station15);
+
+		String station16 = "Mantri Square Sampige Road";
+		System.out.println(station16);
+
+		String station17 = "Nadaprabhu Kempegowda Station (Majestic)";
+		System.out.println(station17);
+
+		String station18 = "Chickpete";
+		System.out.println(station18);
+
+		String station19 = "Krishna Rajendra Market";
+		System.out.println(station19);
+
+		String station20 = "National College";
+		System.out.println(station20);
+
+		String station21 = "Lalbagh";
+		System.out.println(station21);
+
+		String station22 = "South End Circle";
+		System.out.println(station22);
+
+		String station23 = "Jayanagar";
+		System.out.println(station23);
+
+		String station24 = "Rashtriya Vidyalaya Road (R.V. Road)";
+		System.out.println(station24);
+
+		String station25 = "Banashankari";
+		System.out.println(station25);
+
+		String station26 = "Jaya Prakash Nagar (JP Nagar)";
+		System.out.println(station26);
+
+		String station27 = "Yelachenahalli";
+		System.out.println(station27);
+
+		String station28 = "Konanakunte Cross";
+		System.out.println(station28);
+
+		String station29 = "Doddakallasandra";
+		System.out.println(station29);
+
+		String station30 = "Vajarahalli";
+		System.out.println(station30);
+
+		String station31 = "Thalaghattapura";
+		System.out.println(station31);
+
+		String station32 = "Silk Institute";
+		System.out.println(station32);
+
+	}
+}
