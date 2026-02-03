5f34fae0f64738096d93afa179bd15c0fa3d37c0
HarshithaAV82 <avharshitha607@gmail.com>
2026-02-03 11:11:51 +0530

    Add files via upload

---
+++ IndianDance.java
@@ -0,0 +1,101 @@
+class IndianDance{
+	public static void main(String[] args) {
+
+		String dance1 = "Bharatanatyam";
+		System.out.println(dance1);
+
+		String dance2 = "Kathak";
+		System.out.println(dance2);
+
+		String dance3 = "Kathakali";
+		System.out.println(dance3);
+
+		String dance4 = "Kuchipudi";
+		System.out.println(dance4);
+
+		String dance5 = "Odissi";
+		System.out.println(dance5);
+
+		String dance6 = "Manipuri";
+		System.out.println(dance6);
+
+		String dance7 = "Mohiniyattam";
+		System.out.println(dance7);
+
+		String dance8 = "Sattriya";
+		System.out.println(dance8);
+
+		String dance9 = "Bhangra";
+		System.out.println(dance9);
+
+		String dance10 = "Giddha";
+		System.out.println(dance10);
+
+		String dance11 = "Garba";
+		System.out.println(dance11);
+
+		String dance12 = "Dandiya Raas";
+		System.out.println(dance12);
+
+		String dance13 = "Ghoomar";
+		System.out.println(dance13);
+
+		String dance14 = "Lavani";
+		System.out.println(dance14);
+
+		String dance15 = "Bihu";
+		System.out.println(dance15);
+
+		String dance16 = "Chhau";
+		System.out.println(dance16);
+
+		String dance17 = "Yakshagana";
+		System.out.println(dance17);
+
+		String dance18 = "Dollu Kunitha";
+		System.out.println(dance18);
+
+		String dance19 = "Kummi";
+		System.out.println(dance19);
+
+		String dance20 = "Kolattam";
+		System.out.println(dance20);
+
+		String dance21 = "Karagattam";
+		System.out.println(dance21);
+
+		String dance22 = "Theyyam";
+		System.out.println(dance22);
+
+		String dance23 = "Santhali Dance";
+		System.out.println(dance23);
+
+		String dance24 = "Gaur Dance";
+		System.out.println(dance24);
+
+		String dance25 = "Karma Dance";
+		System.out.println(dance25);
+
+		String dance26 = "Hojagiri";
+		System.out.println(dance26);
+
+		String dance27 = "Cheraw (Bamboo Dance)";
+		System.out.println(dance27);
+
+		String dance28 = "Bison Horn Dance";
+		System.out.println(dance28);
+
+		String dance29 = "Wangala Dance";
+		System.out.println(dance29);
+
+		String dance30 = "Rouf";
+		System.out.println(dance30);
+
+		String dance31 = "Perini Shivatandavam";
+		System.out.println(dance31);
+
+		String dance32 = "Veeragase";
+		System.out.println(dance32);
+
+	}
+}
