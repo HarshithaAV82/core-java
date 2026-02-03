beeaa914d213eb190a756e7b2a455067ac03eae7
HarshithaAV82 <avharshitha607@gmail.com>
2026-02-03 11:07:00 +0530

    Add files via upload

---
+++ Friends.java
@@ -0,0 +1,24 @@
+class Friends{
+	public static void main(String[] args){
+		String friend1 = "Harshitha";
+		System.out.println(friend1);
+		String friend2 = "Hema";
+		System.out.println(friend2);
+		String friend3 = "Sonnu";
+		System.out.println(friend3);
+		String friend4 = "Manu";
+		System.out.println(friend4);
+		String friend5 = "Acchu";
+		System.out.println(friend5);
+		String friend6 = "Ammu";
+		System.out.println(friend6);
+		String friend7 = "Hamsika";
+		System.out.println(friend7);
+		String friend8 = "Anjali";
+		System.out.println(friend8);
+		String friend9 = "Habeeba";
+		System.out.println(friend9);
+		String friend10 = "Sanjana";
+		System.out.println(friend10);
+	}
+}
