45b125b4dfcab2ed9597623cca2cc34ab2fc4665
HarshithaAV82 <avharshitha607@gmail.com>
2026-02-03 11:09:16 +0530

    Add files via upload

---
+++ CarCompanies.java
@@ -0,0 +1,74 @@
+class CarCompanies {
+	
+	
+	public static void main(String[] args) {
+
+		String car1 = "BMW";
+		System.out.println(car1);
+		String car2 = "Maruti Suzuki";
+		System.out.println(car2);
+		String car3 = "Audi";
+		System.out.println(car3);
+		String car4 = "Hyundai";
+		System.out.println(car4);
+		String car5 = "Ferrari";
+		System.out.println(car5);
+		String car6 = "Tata Motors";
+		System.out.println(car6);
+		String car7 = "Mercedes-Benz";
+		System.out.println(car7);
+		String car8 = "Mahindra & Mahindra";
+		System.out.println(car8);
+		String car9 = "Lamborghini";
+		System.out.println(car9);
+		String car10 = "Kia";
+		System.out.println(car10);
+		String car11 = "Rolls-Royce";
+		System.out.println(car11);
+		String car12 = "Toyota";
+		System.out.println(car12);
+		String car13 = "Bentley";
+		System.out.println(car13);
+		String car14 = "Honda";
+		System.out.println(car14);
+		String car15 = "Porsche";
+		System.out.println(car15);
+		String car16 = "MG Motor";
+		System.out.println(car16);
+		String car17 = "Maserati";
+		System.out.println(car17);
+		String car18 = "Renault";
+		System.out.println(car18);
+		String car19 = "Aston Martin";
+		System.out.println(car19);
+		String car20 = "Volkswagen";
+		System.out.println(car20);
+		String car21 = "Nissan";
+		System.out.println(car21);
+		String car22 = "Skoda";
+		System.out.println(car22);
+		String car23 = "McLaren";
+		System.out.println(car23);
+		String car24 = "Citroen";
+		System.out.println(car24);
+		String car25 = "Volvo";
+		System.out.println(car25);
+		String car26 = "Jeep";
+		System.out.println(car26);
+		String car27 = "Lexus";
+		System.out.println(car27);
+		String car28 = "Force Motors";
+		System.out.println(car28);
+		String car29 = "Mini";
+		System.out.println(car29);
+		String car30 = "Isuzu";
+		System.out.println(car30);
+		String car31 = "BYD";
+		System.out.println(car31);
+		String car32 = "Jaguar Land Rover";
+		System.out.println(car32);
+		String car33 = "Pravaig";
+		System.out.println(car33);
+
+	}
+}
