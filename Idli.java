aced0f347d557474741584e376e0c5194781273c
Harshitha A . V <avharshitha607@gmail.com>
2026-02-24 16:25:14 +0530

    create class with printing statement

---
+++ Idli.java
@@ -0,0 +1,59 @@
+class Water{
+	public static void main(String[] args){
+		System.out.println("this is class1");
+	}
+}
+
+class Bottle{
+	public static void main(String[] args){
+		System.out.println("this is class2");
+	}
+}
+
+class Drink{
+	public static void main(String[] args){
+		System.out.println("this is class3");
+	}
+}
+
+class Litre{
+	public static void main(String[] args){
+		System.out.println("this is class4");
+	}
+}
+
+class Laptop{
+	public static void main(String[] args){
+		System.out.println("this is class5");
+	}
+}
+
+class Open{
+	public static void main(String[] args){
+		System.out.println("this is class6");
+	}
+}
+
+class Operate{
+	public static void main(String[] args){
+		System.out.println("this is class7");
+	}
+}
+
+class Communication{
+	public static void main(String[] args){
+		System.out.println("this is class8");
+	}
+}
+
+class Perform{
+	public static void main(String[] args){
+		System.out.println("this is class9");
+	}
+}
+
+class Close{
+	public static void main(String[] args){
+		System.out.println("this is class10");
+	}
+}
---
+++ Masala.java
@@ -0,0 +1,59 @@
+class Calculator{
+	public static void main(String[] args){
+		System.out.println("this is class1");
+	}
+}
+
+class Printer{
+	public static void main(String[] args){
+		System.out.println("this is class2");
+	}
+}
+
+class Validator{
+	public static void main(String[] args){
+		System.out.println("this is class3");
+	}
+}
+
+class Convertor{
+	public static void main(String[] args){
+		System.out.println("this is class4");
+	}
+}
+
+class Manager{
+	public static void main(String[] args){
+		System.out.println("this is class5");
+	}
+}
+
+class Processor{
+	public static void main(String[] args){
+		System.out.println("this is class6");
+	}
+}
+
+class Controller{
+	public static void main(String[] args){
+		System.out.println("this is class7");
+	}
+}
+
+class Handler{
+	public static void main(String[] args){
+		System.out.println("this is class8");
+	}
+}
+
+class Exceutor{
+	public static void main(String[] args){
+		System.out.println("this is class9");
+	}
+}
+
+class Checker{
+	public static void main(String[] args){
+		System.out.println("this is class10");
+	}
+}
