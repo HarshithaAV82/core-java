db74754a87a0d1ea21da1fb086fa1d482486e287
HarshithaAV82 <avharsitha607@gmail.com>
2026-05-09 21:52:52 +0530

    practice

---
+++ dsa/src/com/xworkz/pratice/Runner1.java
@@ -0,0 +1,10 @@
+package com.xworkz.pratice;
+
+public class Runner1 {
+
+    public static void main(String[] args) {
+
+        Recursion recursion = new Recursion();
+        System.out.println(recursion.factorial(5));
+    }
+}
