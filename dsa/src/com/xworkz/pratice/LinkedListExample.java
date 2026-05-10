3f70ba604e98a7172429c58f93c7b44dac49c823
HarshithaAV82 <avharsitha607@gmail.com>
2026-05-10 22:09:58 +0530

    practice

---
+++ dsa/src/com/xworkz/pratice/LinkedListExample.java
@@ -0,0 +1,32 @@
+package com.xworkz.pratice;
+
+public class LinkedListExample {
+
+    Node head;
+
+    public void insert(int data){
+
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            Node temp = head;
+
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void display(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
