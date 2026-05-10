0e5e6dd3dc9d5c20e57ea34dbcd70e348952ca12
HarshithaAV82 <avharsitha607@gmail.com>
2026-05-10 20:35:18 +0530

    practice

---
+++ dsa/src/Fibonacci.java
@@ -0,0 +1,22 @@
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int first = 0;
+        int second = 1;
+
+        System.out.println(first);
+        System.out.println(second);
+
+
+        for(int i = 5; i <= 10; i++){
+
+            int next = first + second;
+
+            System.out.println(next);
+
+            first = second;
+            second = next;
+        }
+    }
+}
