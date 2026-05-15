14a1bb5155358fa15e8875420e0504b9de4588d0
HarshithaAV82 <avharsitha607@gmail.com>
2026-05-15 20:00:00 +0530

    Maintain streak

---
+++ concept/src/com/xworkz/small/Employee.java
@@ -0,0 +1,11 @@
+package com.xworkz.small;
+
+public abstract class Employee {
+
+    protected abstract void work();
+
+    void login() {
+        System.out.println("Employee logged in");
+    }
+}
+
