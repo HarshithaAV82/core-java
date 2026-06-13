0505142e8274845a7bd03107e7086f0a8a4df8eb
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-13 19:25:49 +0530

    practice

---
+++ Collection/CollectionPractice/src/com/xworkz/collectionPractice/ArrayListExample.java
@@ -0,0 +1,18 @@
+package com.xworkz.collectionPractice;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Harshitha");
+        names.add("Manasa");
+        names.add("Anu");
+
+        System.out.println(names);
+    }
+}
