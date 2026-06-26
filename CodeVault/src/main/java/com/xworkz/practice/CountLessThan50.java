b18b3011d3853cec62a16cd99c85181723728314
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-26 17:13:40 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/CountLessThan50.java
+++ CodeVault/src/main/java/com/xworkz/practice/CountLessThan50.java
@@ -8,24 +8,32 @@ public class CountLessThan50 {
 
         Scanner scanner = new Scanner(System.in);
 
+        //Step 1 : Read n
         int n = scanner.nextInt();
 
+        //Step 2 : Create array
         int[] arr = new int[n];
 
+        //Step 3 : Store array elements
         for (int i = 0; i < n; i++){
             arr[i] = scanner.nextInt();
         }
 
+        //Step 4 : Create count variable
         int count = 0;
 
+        //Step 5 : Traverse array
         for (int i = 0; i < n; i++){
 
+            //Step 6 : Check whether element is less than 50
             if(arr[i] < 50){
 
+                //Step 7 : Increment count
                 count++;
             }
         }
 
+        //Step 8 : Print count
         System.out.println("Number of elements smaller than 50:" + count);
     }
 }
