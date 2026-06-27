97a5770ab1b30fa56957d496523b417b20341381
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-27 21:47:02 +0530

    practice

---
+++ CodeVault/src/main/java/com/xworkz/practice/ReverseArray.java
@@ -0,0 +1,32 @@
+package com.xworkz.practice;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Step 1: Read n
+        System.out.println("Enter the number of array elements");
+        int n = scanner.nextInt();
+
+        // Step 2: Create array
+        int[] arr = new int[n];
+
+        // Step 3: Store array elements
+        System.out.println("Enter the array elements");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Step 4: Traverse array from last index
+        for (int i = n - 1; i >= 0; i--) {
+
+            // Step 5: Print elements
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
