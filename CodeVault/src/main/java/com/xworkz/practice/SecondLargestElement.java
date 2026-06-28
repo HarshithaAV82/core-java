a8aa1390a3aeb7a2d994697c6786127d44514af8
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-28 11:54:05 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/SecondLargestElement.java
+++ CodeVault/src/main/java/com/xworkz/practice/SecondLargestElement.java
@@ -8,11 +8,14 @@ public class SecondLargestElement {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read n
         System.out.println("Enter the number of array elements");
         int n = scanner.nextInt();
 
+        // Step 2: Create array
         int[] arr = new int[n];
 
+        // Step 3: Store array elements
         System.out.println("Enter the array elements");
 
         for(int i = 0; i < n; i++){
@@ -20,23 +23,30 @@ public class SecondLargestElement {
             arr[i] = scanner.nextInt();
         }
 
+        // Step 4: Assume first element is largest and secondLargest
         int largest = arr[0];
         int secondLargest = arr[0];
 
+        // Step 5: Traverse array
         for (int i = 1; i < n;  i++){
 
+            // Step 6: Compare with largest
             if(arr[i] > largest){
 
+                // Step 7: Update largest and secondLargest
                 secondLargest = largest;
                 largest = arr[i];
 
-            } else if (arr[i] > secondLargest && arr[i] != largest){
+            }
+            // Step 8: Compare with secondLargest
+            else if (arr[i] > secondLargest && arr[i] != largest){
 
-
+                // Step 9: Update secondLargest
                     secondLargest = arr[i];
 
                 }
             }
+        // Step 10: Print secondLargest
             System.out.println("Second Largest Element:" + secondLargest);
         }
     }
