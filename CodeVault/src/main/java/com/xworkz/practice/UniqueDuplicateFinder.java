ad03a1be7a96a687b363a4ec5d21f92c34256642
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-29 20:59:10 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/UniqueDuplicateFinder.java
+++ CodeVault/src/main/java/com/xworkz/practice/UniqueDuplicateFinder.java
@@ -9,22 +9,30 @@ public class UniqueDuplicateFinder {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read n
         System.out.println("Enter the number of array elements");
         int n = scanner.nextInt();
 
+        // Step 2: Create array
         int[] arr = new int[n];
 
+
+        // Step 3: Store array elements
         System.out.println("Enter the array elements");
+
 
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
         System.out.println("Duplicate Elemnets:");
 
+        // Step 4: Traverse the array
         for (int i = 0; i < n; i++) {
 
+            // Step 5: Assume current element is not printed
             boolean alreadyPrinted = false;
 
+            // Step 6: Check whether current element already appeared
             for (int k = 0; k < i; k++) {
                 if (arr[i] == arr[k]){
                     alreadyPrinted = true;
@@ -32,13 +40,18 @@ public class UniqueDuplicateFinder {
                 }
             }
 
+            // Step 7: Skip if duplicate is already printed
             if (alreadyPrinted){
                 continue;
             }
 
+            // Step 8: Compare current element with remaining elements
             for (int j = i + 1; j < n; j++){
 
+                // Step 9: Check duplicate
                 if (arr[i] == arr[j]){
+
+                    // Step 10: Print duplicate element only once
                     System.out.println(arr[i]);
                     break;
                 }
