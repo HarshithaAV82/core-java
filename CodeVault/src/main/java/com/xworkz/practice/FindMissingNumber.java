b2f09ae903af04c86d0aae8fdde303d60d181fb0
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-30 18:19:34 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/FindMissingNumber.java
+++ CodeVault/src/main/java/com/xworkz/practice/FindMissingNumber.java
@@ -8,11 +8,14 @@ public class FindMissingNumber {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read n
         System.out.println("Enter the number of array elements");
         int n = scanner.nextInt();
 
+        // Step 2: Create array
         int[] arr = new int[n];
 
+        // Step 3: Store array elements
         System.out.println("Enter the array elements");
 
         int actualSum = 0;
@@ -20,13 +23,17 @@ public class FindMissingNumber {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
 
+            // Step 4: Calculate actual sum
             actualSum = actualSum + arr[i];
         }
 
+        // Step 5: Calculate expected sum
         int expectedSum = (n + 1) * (n + 2) / 2;
 
+        // Step 6: Find missing number
         int missingNumber = expectedSum - actualSum;
 
+        // Step 7: Print missing number
         System.out.println("Missing Number:" + missingNumber);
     }
 }
