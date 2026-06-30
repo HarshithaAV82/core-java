c9a43ff788a99aeed604ab68f1bcfe431efe9488
HarshithaAV82 <avharsitha607@gmail.com>
2026-06-30 19:00:55 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/ArrayPalindrome.java
+++ CodeVault/src/main/java/com/xworkz/practice/ArrayPalindrome.java
@@ -8,26 +8,34 @@ public class ArrayPalindrome {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read n
         System.out.println("Enter the number of array elements");
         int n = scanner.nextInt();
 
+        // Step 2: Create array
         int[] arr = new int[n];
 
+        // Step 3: Store array elements
         System.out.println("Enter the array elements");
         for (int i = 0; i < n; i++){
             arr[i] = scanner.nextInt();
         }
 
+        // Step 4: Assume the array is palindrome
         boolean isPalindrome = true;
 
+        // Step 5: Traverse only half of the array
         for (int i = 0; i < n/2; i++){
 
+            // Step 6: Compare first and last elements
             if (arr[i] != arr[n - 1 - i]){
 
+                // Step 7: Not a palindrome
                 isPalindrome = false;
                 break;
             }
         }
+        // Step 8: Print the result
         if(isPalindrome){
             System.out.println("Array is palindrome");
         }
