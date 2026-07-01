677ca7b0d14645c361a6a50dd89b01ce2a41fd17
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-01 18:15:47 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/SortArrayAscending.java
+++ CodeVault/src/main/java/com/xworkz/practice/SortArrayAscending.java
@@ -8,13 +8,14 @@ public class SortArrayAscending {
 
         Scanner scanner = new Scanner(System.in);
 
-
+        // Step 1: Read n
         System.out.println("Enter the number of array elements");
         int n = scanner.nextInt();
 
+        // Step 2: Create array
         int[] arr = new int[n];
 
-
+        // Step 3: Store array elements
         System.out.println("Enter the array elements");
 
         for (int i = 0; i < n; i++){
@@ -22,12 +23,16 @@ public class SortArrayAscending {
             arr[i] = scanner.nextInt();
         }
 
+        // Step 4: Traverse array
         for (int i = 0; i < n; i++){
 
+            // Step 5: Compare remaining elements
             for (int j = i + 1; j < n; j++){
 
+                // Step 6: Compare
                 if (arr[i] > arr[j]){
 
+                    // Step 7: Swap
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
@@ -35,6 +40,7 @@ public class SortArrayAscending {
             }
         }
 
+        // Step 8: Print sorted array
         System.out.println("Array in Ascending Order:");
 
         for (int i = 0; i < n; i++){
