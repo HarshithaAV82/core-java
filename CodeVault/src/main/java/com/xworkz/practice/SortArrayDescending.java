736f71537a9f2c8dd725c0f62da9449bcbc67f93
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-01 18:25:27 +0530

    practice

---
+++ CodeVault/src/main/java/com/xworkz/practice/SortArrayDescending.java
@@ -0,0 +1,42 @@
+package com.xworkz.practice;
+
+import java.util.Scanner;
+
+public class SortArrayDescending {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of array elements");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements");
+
+        for (int i = 0; i < n; i++){
+
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i =0; i < n; i++){
+
+            for (int j = i + 1; j < n; j++){
+
+                if (arr[i] < arr[j]){
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Array in Descending Order:");
+
+        for (int i = 0; i < n; i++){
+            System.out.println(arr[i] + " ");
+        }
+           }
+}
