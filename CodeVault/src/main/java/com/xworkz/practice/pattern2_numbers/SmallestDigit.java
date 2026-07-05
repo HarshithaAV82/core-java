50c05bfc09e8a33617debc41592fa8b5c56bbeb8
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-05 21:35:32 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/SmallestDigit.java
+++ CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/SmallestDigit.java
@@ -7,23 +7,31 @@ public class SmallestDigit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read the number
         System.out.println("Enter a number");
         int num = scanner.nextInt();
 
+        // Step 2: Create smallest variable
         int smallest = 9;
 
+        // Step 3: Repeat until number becomes 0
         while (num != 0){
 
+            // Step 4: Find last digit
             int digit = num % 10;
 
+            // Step 5: Compare digit with smallest
             if (digit < smallest){
 
+                // Step 6: Update smallest
                 smallest = digit;
             }
 
+            // Step 7: Remove last digit
             num = num / 10;
         }
 
+        // Step 8: Print smallest digit
         System.out.println("Smallest Digit:" + smallest);
     }
 }
