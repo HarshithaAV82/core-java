4cc9e0872b55c26c3c92c01ab366a9fe9a3b50ad
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-05 20:58:57 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/ProductOfDigits.java
+++ CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/ProductOfDigits.java
@@ -8,20 +8,27 @@ public class ProductOfDigits {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read the number
         System.out.println("Enter a number");
         int number = scanner.nextInt();
 
+        // Step 2: Create product variable
         int product = 1;
 
+        // Step 3: Repeat until number becomes 0
         while (number != 0){
 
+            // Step 4: Find last digit
             int digit = number % 10;
 
+            // Step 5: Multiply digit with product
             product = product * digit;
 
+            // Step 6: Remove last digit
             number = number / 10;
         }
 
+        // Step 7: Print product
         System.out.println("Product of digits:" + product);
     }
 }
