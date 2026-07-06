9ce8b89c881e27f3447a0501a0a8eaeb5133a276
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-06 21:20:57 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/CountEvenDigits.java
+++ CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/CountEvenDigits.java
@@ -8,23 +8,31 @@ public class CountEvenDigits {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read the number
         System.out.println("Enter a number");
         int num = scanner.nextInt();
 
+        // Step 2: Create count variable
         int count = 0;
 
+        // Step 3: Repeat until number becomes 0
         while (num != 0){
 
+            // Step 4: Find last digit
             int digit = num % 10;
 
+            // Step 5: Check whether digit is even
             if(digit % 2 == 0){
 
+                // Step 6: Increment count
                 count++;
             }
 
+            // Step 7: Remove last digit
             num = num / 10;
         }
 
+        // Step 8: Print count
         System.out.println("Number of even digits:" + count);
     }
 }
