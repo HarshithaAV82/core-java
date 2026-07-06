c737f47ea8f40e3edde53201d26d64136a0be1ac
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-06 20:19:23 +0530

    practice

--- CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/CountDigits.java
+++ CodeVault/src/main/java/com/xworkz/practice/pattern2_numbers/CountDigits.java
@@ -8,16 +8,21 @@ public class CountDigits {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read the number
         System.out.println("Enter a number");
         int num = scanner.nextInt();
 
+        // Step 2: Create count variable
         int count = 0;
 
+        // Step 3: Repeat until number becomes 0
         while (num != 0){
 
+            // Step 4: Remove last digit
             num = num/10;
             count++;
         }
+        // Step 6: Print count
         System.out.println("Number of digits:" + count);
     }
 }
