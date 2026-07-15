9748a12a100dcb3d5a597014ff8ae1026418eadf
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-15 21:09:07 +0530

    practice

--- javapractice/src/com/xworkz/javaprograms/CountDigitsInString.java
+++ javapractice/src/com/xworkz/javaprograms/CountDigitsInString.java
@@ -8,20 +8,28 @@ public class CountDigitsInString {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Step 1: Read the string
         System.out.println("Enter a String");
         String word = scanner.nextLine();
 
+        // Step 2: Create count variable
         int count = 0;
 
+        // Step 3: Traverse the string
         for (int i = 0; i < word.length(); i++){
 
+            // Step 4: Get each character
             char ch = word.charAt(i);
 
+            // Step 5: Check whether it is a digit
             if (ch >= '0' && ch <= '9'){
+
+                // Step 6: Increment count
                 count++;
             }
         }
 
+        // Step 7: Print result
         System.out.println("Number of Digits:" + count);
     }
 }
