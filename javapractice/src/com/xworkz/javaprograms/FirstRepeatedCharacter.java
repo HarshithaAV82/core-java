0bd175f2b5e213544a02c1974df9d7ca9a6ba1fa
HarshithaAV82 <avharsitha607@gmail.com>
2026-07-23 16:59:46 +0530

    practice

--- javapractice/src/com/xworkz/javaprograms/FirstRepeatedCharacter.java
+++ javapractice/src/com/xworkz/javaprograms/FirstRepeatedCharacter.java
@@ -6,22 +6,28 @@ public class FirstRepeatedCharacter {
 
     public static void main(String[] args) {
 
+        // Step 1: Create Scanner object
         Scanner scanner = new Scanner(System.in);
 
+        // Step 2: Read the string
         System.out.println("Enter a string");
         String word = scanner.nextLine();
 
+        // Step 3: Create an empty string
         String result = "";
 
+        // Step 4: Traverse the string
         for(int i = 0; i < word.length(); i++){
             char ch = word.charAt(i);
 
+            // Step 5: Check whether character already exists
             if (result.contains(String.valueOf(ch))){
 
                 System.out.println("First Repeated Character:" + ch);
                 return;
             }
-            result = result + ch;
+            // Step 6: Add character to result
+            result = result + ch; //result.append(ch);
         }
 
         System.out.println("No Repeated Character Found");
