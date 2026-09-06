ffd64b55220c3e42c3871d2b516e7d70f2a1849a
HarshithaAV82 <avharsitha607@gmail.com>
2026-09-06 18:17:23 +0530

    Add Stream API GroupingBySuming example

---
+++ javapractice/src/com/xworkz/javaprograms/Employee4.java
@@ -0,0 +1,32 @@
+package com.xworkz.javaprograms;
+
+public class Employee4 {
+
+    private int id;
+    private  String name;
+    private  String department;
+    private  int salary;
+
+    public Employee4(int id, String name, String department, int salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
