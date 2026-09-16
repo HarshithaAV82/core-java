70dc22d8c54b473605f70ededfe990fd725adeed
HarshithaAV82 <avharsitha607@gmail.com>
2026-09-16 16:31:00 +0530

    Add ExecutorService fixed thread pool example

---
+++ multithreadingadvance/multithreadingadvance/src/com/xworkz/practicemultithreadingconcepts/FixedThreadPoolExample.java
@@ -0,0 +1,35 @@
+package com.xworkz.practicemultithreadingconcepts;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPoolExample {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i= 0; i <= 5; i++) {
+
+            int taskNumber = i;
+
+            executorService.submit(() -> {
+
+                System.out.println("Task" + taskNumber + "is executed by" + Thread.currentThread().getName());
+
+                try {
+
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+
+        }
+
+        executorService.shutdown();
+
+        System.out.println("All tasks submitted");
+
+    }
+}
